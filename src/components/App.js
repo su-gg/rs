aff0e1b32e33e02bf0b2e853c7be89fda9d91fe2
su-gg <suzanne.gonzalezgarcia@gmail.com>
2024-12-20 12:32:32 +0100

    Add files via upload

---
+++ src/components/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "../styles/App.css";
+
+function App() {
+    return (
+        <div className="container">
+            <h1>Site en cours de construction</h1>
+            <p>Merci de votre patience. Nous travaillons activement pour mettre en ligne notre site.</p>
+            <div className="loader"></div>
+        </div>
+    );
+}
+
+export default App;
